291d8217d357899b13c53a96f270daf7c84d6373
hoochicken <mareike.riegel@ql.de>
2022-10-15 12:22:54 +0200

    initial commit

---
+++ php/fields/checkpluginsystemqldebug.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package		mod_qlform
+ * @copyright	Copyright (C) 2017 ql.de All rights reserved.
+ * @author 		Mareike Riegel mareike.riegel@ql.de
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+jimport('joomla.html.html');
+jimport('joomla.form.formfield');
+
+class JFormFieldCheckpluginsystemqldebug extends JFormField
+{
+    /**
+     * The form field type.
+     *
+     * @var  string
+     * @since 1.6
+     */
+    protected $type = 'checkpluginsystemqldebug'; //the form field type see the name is the same
+
+    /**
+     * Method to retrieve the lists that resides in your application using the API.
+     *
+     * @return array The field option objects.
+     * @since 1.6
+     */
+    protected function getInput()
+    {
+        if(false==$this->checkIfPluginExists('system','qldebug')) return JFactory::getApplication()->enqueueMessage(JText::_('MOD_QLDEBUG_MSG_PLUGINNOTFOUNDORCONFIGURATED'),'error');
+    }
+    protected function checkIfPluginExists($type,$strPlugin)
+    {
+        $plg=JPluginHelper::getPlugin($type,$strPlugin);
+        if(0<count($plg))return true;
+        return false;
+    }
+}
