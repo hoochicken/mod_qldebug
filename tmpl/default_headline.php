00f1cf9fbfb78687fe24b291bb8a2e9b1bf59fe6
hoochicken <mareike.riegel@ql.de>
2022-10-18 16:08:31 +0200

    feat (updater) doc adjusted

--- tmpl/default_headline.php
+++ tmpl/default_headline.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package		plg_system_qldebug
+ * @package		mod_qldebug
  * @copyright	Copyright (C) 2022 ql.de All rights reserved.
  * @author 		Mareike Riegel mareike.riegel@ql.de
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
